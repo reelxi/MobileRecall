1c7cbb467cef560f1cf31ad10933e8438ad4c2fc
R. Elbracht <elbrachtrene@gmail.com>
2023-11-25 01:13:30 +0100

    add configuration: mapper and cors

--- mobile_recall-backend/src/main/java/mobile_recall/Configuration/CorsConfiguration.java
+++ mobile_recall-backend/src/main/java/mobile_recall/Configuration/CorsConfiguration.java
@@ -6,12 +6,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-
-        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
-
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
     }
-
 }
